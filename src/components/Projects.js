b273e6b1ad1ad35700d6b1ce435edcf6e16c5ad0
sai20459 <83965920+sai20459@users.noreply.github.com>
2024-08-09 10:20:52 +0100

    bug fixes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -187,6 +187,172 @@ const Projects = () => {
               /> */}
             </div>
           </li>
+          <li className="mb-12">
+            <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+              <div className="z-10 sm:order-2 sm:col-span-6">
+                <h3 className="font-medium leading-snug text-slate-200">
+                  {/* Web Developer{" "} */}
+                  <a
+                    className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                    // href="https://prequalification.i4strategies.com/"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                    Tyuts
+                  </a>
+                </h3>
+                <p className="mt-2 text-sm leading-normal">
+                  A fully interactive tutorial application where teachers can
+                  create courses and learners can learn & track progress like
+                  Udemy
+                </p>
+                <ul className="mt-2 flex flex-wrap">
+                  {["React.js", "Javascript", "Antd", "Next.js"].map(
+                    (item, index) => (
+                      <li className="mr-1.5 mt-2" key={index}>
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                          {item}
+                        </div>
+                      </li>
+                    )
+                  )}
+                </ul>
+              </div>
+              {/* <img
+                src=""
+                className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+              /> */}
+            </div>
+          </li>
+          <li className="mb-12">
+            <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+              <div className="z-10 sm:order-2 sm:col-span-6">
+                <h3 className="font-medium leading-snug text-slate-200">
+                  {/* Web Developer{" "} */}
+                  <a
+                    className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                    // href="https://prequalification.i4strategies.com/"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                    Stock Management System
+                  </a>
+                </h3>
+                <p className="mt-2 text-sm leading-normal">
+                  The project's major goal was to keep track of inventory in a
+                  database by scanning QR codes on products. When the stock
+                  expires, it will notify the owner and give him a daily update
+                  in pdf format.
+                  <br />
+                  My role in the project was to keep the design clean and
+                  include a QR code scanner.
+                </p>
+                {/* <ul className="mt-2 flex flex-wrap">
+                  {["React.js", "Javascript", "Antd", "Next.js"].map(
+                    (item, index) => (
+                      <li className="mr-1.5 mt-2" key={index}>
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                          {item}
+                        </div>
+                      </li>
+                    )
+                  )}
+                </ul> */}
+              </div>
+              {/* <img
+                src=""
+                className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+              /> */}
+            </div>
+          </li>
+          <li className="mb-12">
+            <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+              <div className="z-10 sm:order-2 sm:col-span-6">
+                <h3 className="font-medium leading-snug text-slate-200">
+                  {/* Web Developer{" "} */}
+                  <a
+                    className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                    // href="https://prequalification.i4strategies.com/"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                    Crop Protection System From Animals
+                  </a>
+                </h3>
+                <p className="mt-2 text-sm leading-normal">
+                  The focus of the project was based on surveillance with an
+                  animal ward-off system employed in farmlands in order to
+                  prevent agricultural lands vandalization by animals.
+                  <br />
+                  My role in the project team includes C coding, PIC
+                  microcontroller and Wifi moduleIntegrations.
+                </p>
+                {/* <ul className="mt-2 flex flex-wrap">
+                  {["React.js", "Javascript", "Antd", "Next.js"].map(
+                    (item, index) => (
+                      <li className="mr-1.5 mt-2" key={index}>
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                          {item}
+                        </div>
+                      </li>
+                    )
+                  )}
+                </ul> */}
+              </div>
+              {/* <img
+                src=""
+                className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+              /> */}
+            </div>
+          </li>
+          <li className="mb-12">
+            <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+              <div className="z-10 sm:order-2 sm:col-span-6">
+                <h3 className="font-medium leading-snug text-slate-200">
+                  {/* Web Developer{" "} */}
+                  <a
+                    className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                    // href="https://prequalification.i4strategies.com/"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                    Event Planner using PHP and JavaScript
+                  </a>
+                </h3>
+                <p className="mt-2 text-sm leading-normal">
+                  A web page for event planning in a user-friendly way. Services
+                  of event planning are collectively accessible here. People can
+                  select the services they want to be handled by their choice
+                  <br />
+                  This is my individual project and I dealt with HTML, CSS, PHP
+                  and Javascript.
+                </p>
+                {/* <ul className="mt-2 flex flex-wrap">
+                  {["React.js", "Javascript", "Antd", "Next.js"].map(
+                    (item, index) => (
+                      <li className="mr-1.5 mt-2" key={index}>
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                          {item}
+                        </div>
+                      </li>
+                    )
+                  )}
+                </ul> */}
+              </div>
+              {/* <img
+                src=""
+                className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+              /> */}
+            </div>
+          </li>
         </ol>
       </div>
     </section>
