cd6bc7b80f2da595f3da4070bf4bbf3673a1c3a4
sai20459 <83965920+sai20459@users.noreply.github.com>
2024-10-17 22:58:37 +0100

    firebase api keys

--- src/analytics.js
+++ src/analytics.js
@@ -2,24 +2,24 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 export const createFirebaseApp = () => {
-  const clientCredentials = {
-    apiKey: process?.env?.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process?.env?.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process?.env?.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process?.env?.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process?.env?.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process?.env?.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process?.env?.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  const firebaseConfig = {
+    apiKey: "AIzaSyCaNssll_uJJx6KoEpUhpSK5LcMlj3_Osw",
+    authDomain: "websi-be52f.firebaseapp.com",
+    projectId: "websi-be52f",
+    storageBucket: "websi-be52f.appspot.com",
+    messagingSenderId: "255691819679",
+    appId: "1:255691819679:web:6d094bba01c03e398cb0aa",
+    measurementId: "G-EYQ06EGPPJ",
   };
 
   //   const app = initializeApp(firebaseConfig);
   // const analytics = getAnalytics(app);
   if (getApps().length <= 0) {
-    const app = initializeApp(clientCredentials);
+    const app = initializeApp(firebaseConfig);
     // Check that `window` is in scope for the analytics module!
     if (typeof window !== "undefined") {
       // Enable analytics. https://firebase.google.com/docs/analytics/get-started
-      if ("measurementId" in clientCredentials) {
+      if ("measurementId" in firebaseConfig) {
         getAnalytics();
       }
     }
